6f1f8b796c2898c9d851b6b4dce95f43873ab44b
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-19 12:37:39 -0500

    add decorators to login controller

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { get } from './decorators/routes';
+import { controller } from './decorators/controller';
 
-@controller('/')
-class LoginController {
+@controller('/auth')
+export class LoginController {
   @get('/login')
   getLogin(req: Request, res: Response):void {
     res.send(`
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response} from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['klkl']}));
 
 app.use(router);
+app.use(controllerRouter);
 
 // app.get('/', (req: Request, res: Response) => {
 //   res.send(`
